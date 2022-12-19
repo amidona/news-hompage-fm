ac4df74e028e40aa6b0db5e3294bd7cc34727461
Allison Amidon <93014228+amidona@users.noreply.github.com>
2022-12-19 15:17:29 -0600

    Set up hover links

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
-const nav = document.querySelector("nav");
+const button = document.querySelector("button");
+const link = document.querySelectorAll("a");
+const navItem = document.querySelectorAll(".nav-item");
 
+//Open nav menu on mobile
 open.addEventListener("click", function () {
-    console.log("It's working!");
-    document.body.classList.add("open")
+    document.body.classList.add("open");
 });
 
+//Close nav menu on mobile using the X
 close.addEventListener("click", function () {
-    console.log("I'm working too!")
-    document.body.classList.remove("open")
+    document.body.classList.remove("open");
 });
+
+//Close nav menu on mobile when you click to go to a section
+navItem.forEach(navItem => navItem.addEventListener("click", function () {
+    if (document.body.classList.contains("open")) {
+        document.body.classList.remove("open")
+    } else {
+        return
+    }
+}));
+
+//Hover over Read More link to the #Popular article
+button.addEventListener("mouseover", function() {
+    button.classList.add("button-click");
+});
+button.addEventListener("mouseout", function () {
+    button.classList.remove("button-click");
+});
+
+//Hover over links to the other articles
+link.forEach(link => link.addEventListener("mouseover", function() {
+    link.classList.add("hover");
+}));
+link.forEach(link => link.addEventListener("mouseout", function() {
+    link.classList.remove("hover");
+}));
+
